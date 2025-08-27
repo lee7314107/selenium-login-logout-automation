03081bebbf6209b4610dcec1573c24c049c445f0
lee7314107 <lee7314107@gmail.com>
2025-08-27 16:57:29 +0900

    Update login_logout.py

--- login_logout.py
+++ login_logout.py
@@ -13,19 +13,19 @@ options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--user-data-dir=C:/emp/selenium") 
 
 # ChromeDriver 드라이버 경로
-chrome_driver_path = "C:/python/chromedriver-win64/chromedriver.exe"
+chrome_driver_path = "C:/python/chromedriver-win64/chromedriver.exe" #chromedriver 필수로 추가
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
 # 로그인 페이지 접속
 driver.get("https://the-internet.herokuapp.com/login")
-time.sleep(2)
+time.sleep(2) #2초 대기
 
 # 로그인 정보 입력
 driver.find_element(By.ID, "username").send_keys("tomsmith")
-time.sleep(1)
+time.sleep(1) #1초 대기
 driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
-time.sleep(1)
+time.sleep(1) #1초 대기
 driver.find_element(By.CSS_SELECTOR, "button.radius").click()
 
 # 로그인 성공 메시지 기다리기
@@ -38,7 +38,7 @@ success_message = driver.find_element(By.ID, "flash").text
 if "You logged into a secure area!" in success_message:
     print("Login Test Passed")
     driver.execute_script("alert('로그인 성공!')")
-    time.sleep(2)
+    time.sleep(2) #2초 대기
     driver.switch_to.alert.accept()
 else:
     print("Login Test Failed")
@@ -47,17 +47,17 @@ else:
 WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.button.secondary.radius'))
 )
-time.sleep(2)
+time.sleep(2) #2초 대기
 
 # 로그아웃 클릭
 driver.find_element(By.CSS_SELECTOR, 'a.button.secondary.radius').click()
-time.sleep(5)
+time.sleep(5) #5초 대기
 
 # 로그아웃 메시지 확인
 WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located((By.ID, "flash"))
 )
-time.sleep(5)
+time.sleep(5) #5초 대기
 
 # 로그아웃 성공 확인
 if "You logged out of the secure area!" in driver.page_source:
